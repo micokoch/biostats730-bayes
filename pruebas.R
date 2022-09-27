7b969706de7da260e8e356a6f4aacbd08b338f86
Alvaro J. Castro Rivadeneira <acastrorivad@umass.edu>
2022-09-27 14:56:22 -0400

    update class files

--- pruebas.R
+++ pruebas.R
@@ -25,4 +25,43 @@ set.seed(1234)
 (y.bar <- (theta.bar + r.bar))
 (z.bar <- mean(z))
 
+### HW 3
 
+post.iqs %>% 
+  ggplot(aes(x = b_Intercept)) + 
+  geom_histogram(bins=100) + 
+  theme_bw()
+
+## Lucas code
+ratio_hat <- (posterior_samples(fit,   pars= 'b_Intercept')/posterior_samples(fit,   pars= 'sigma'))[[1]]
+mean(ratio_hat)
+quantile(ratio_hat,c(.1,.9)) 
+
+
+fit_df <- as_draws_df(fit)
+x <- mutate_variables(fit_df, ratio = b_Intercept/sigma)
+x <- subset_draws(x, 'ratio')
+posterior_summary(x, probs = c(.1,.9), variable= 'ratio')
+
+summarise_draws(x)
+
+## INLA - https://www.r-inla.org/
+# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+inla.upgrade() # for the stable version
+
+# Test example
+library(INLA)
+n = 100; a = 1; b = 1; tau = 100
+z = rnorm(n)
+eta = a + b*z
+
+scale = exp(rnorm(n))
+prec = scale*tau
+y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
+
+
+data = list(y=y, z=z)
+formula = y ~ 1+z
+result = inla(formula, family = "gaussian", data = data)
+
+summary(result)
